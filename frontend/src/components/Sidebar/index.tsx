bb1cf660a68096b8b11c9a2048afcd4cda2aa3df
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2024-10-20 16:52:13 -0500

    Refactorizar gestión de menú lateral para admitir submenús

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,30 +1,22 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { RolesExisting } from "../../types/Roles";
-import {Menus} from "../../data/Roles";
+import { Menus } from "../../data/Roles";
+import { SubMenuNav } from "./SubMenuNav";
+import { RegularNav } from "./RegularNav";
 
 interface Props {
   role: RolesExisting;
 }
-const Sidebar = ({role}: Props) => {
+const Sidebar = ({ role }: Props) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  // Estado para manejar múltiples submenús
-  const [openSections, setOpenSections] = useState<{ [key: string]: boolean }>({});
 
   const Menu = Menus[role];
 
-  console.log('Menu', Menu);
+  console.log("Menu", Menu);
 
   const handleClickToggle = () => {
     setIsSidebarOpen(!isSidebarOpen);
-  };
-
-  // Función para alternar cualquier sección
-  const toggleSection = (section: string) => {
-    setOpenSections((prevState) => ({
-      ...prevState,
-      [section]: !prevState[section],
-    }));
   };
 
   return (
@@ -38,66 +30,47 @@ const Sidebar = ({role}: Props) => {
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <div className="flex items-center justify-center py-4 mb-4">
-            <img src="/images/logos/maslogo.webp" alt="Logo MAS" width="150px" height="80px" />
+            <img
+              src="/images/logos/maslogo.webp"
+              alt="Logo MAS"
+              width="150px"
+              height="80px"
+            />
           </div>
           <ul className="space-y-2 font-medium">
-            {
-              Menu.map((item, index) => {
-                if (item.subMenu) {
-                  return (
-                    <li key={index}>
-                      <button
-                        onClick={() => toggleSection(item.name)}
-                        className="flex items-center justify-between w-full px-4 py-2 text-sm font-medium text-gray-600 rounded-lg dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                      >
-                        <span>{item.name}</span>
-                        <svg
-                          className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            clipRule="evenodd"
-                            fillRule="evenodd"
-                            d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                          ></path>
-                        </svg>
-                      </button>
-                      <ul
-                        className={`${
-                          openSections[item.name] ? "block" : "hidden"
-                        } pl-4 mt-2 space-y-1`}
-                      >
-                        {item.subMenu.map((subitem, index) => (
-                          <li key={index}>
-                            <NavLink
-                              to={subitem.path}
-                              className="block px-4 py-2 text-sm text-gray-600 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                            >
-                              {subitem.name}
-                            </NavLink>
-                          </li>
-                        ))}
-                      </ul>
-                    </li>
-                  );
-                } else {
-                  return (
-                    <li key={index}>
-                      <NavLink
-                        to={item.path}
-                        className="block px-4 py-2 text-sm text-gray-600 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus
-                        :ring-gray-600"
-                      >
-                        {item.name}
-                      </NavLink>
-                    </li>
-                  );
-                }
-              })
-            }
+            {Menu.map((item, index) => {
+              if (item.subMenu) {
+                return <SubMenuNav key={index} item={item} />;
+              } else {
+                return <RegularNav key={index} item={item} />;
+              }
+            })}
           </ul>
+          <div>
+            <NavLink
+              to="/logout"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <svg
+                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 16"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                />
+              </svg>
+              <span className="flex-1 ms-3 whitespace-nowrap">
+                Cerrar Sesión
+              </span>
+            </NavLink>
+          </div>
         </div>
       </aside>
       <button
