6d90735414abeb524b86d60a10ae3e339a07f31c
Daniel Amado <daniel.amadove@gmail.com>
2024-10-21 15:28:17 -0500

    Refactorizar página de error para agregar enlaces de inicio y sesión

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,14 +1,25 @@
 // src/pages/ErrorPage.js
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
       <h1 className="text-8xl text-white mb-4">Error</h1>
       <h2 className="text-4xl text-white mb-4">Página no encontrada</h2>
-      <Link to="/" className="text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-        Volver al Inicio
-      </Link>
+      <div className="flex gap-2">
+        <Link
+          to="/"
+          className="text-white border border-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Volver al Inicio
+        </Link>
+        <Link
+          to="/login"
+          className="text-white bg-primary focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Iniciar sesión
+        </Link>
+      </div>
     </div>
   );
 };
