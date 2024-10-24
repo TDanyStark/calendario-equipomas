4e2037fb9fa860589ff6f2ef6189dd1e5c866a4b
Daniel Amado <daniel.amadove@gmail.com>
2024-10-24 10:23:02 -0500

    Refactorizar estilos del botón primario y del título, tabla de instrumentos y paginación

--- frontend/src/pages/Instruments.tsx
+++ frontend/src/pages/Instruments.tsx
@@ -5,8 +5,8 @@ import axios from "axios";
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useSelector } from "react-redux"; 
-import { RootState } from "../store/store"; 
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 import { URL_BACKEND } from "../variables";
 import { Loader } from "../components/Loader/Loader";
 
@@ -29,7 +29,6 @@ import {
   CellSelect,
 } from "@table-library/react-table-library/select";
 import { usePagination } from "@table-library/react-table-library/pagination";
-
 
 // Tipo para el instrumento
 type Instrument = {
@@ -60,11 +59,9 @@ const Instruments = () => {
     data: instruments,
     isLoading,
     isError,
-  } = useQuery(
-    "instruments",
-    () => fetchInstruments(token),
-    { enabled: !!token } 
-  );
+  } = useQuery("instruments", () => fetchInstruments(token), {
+    enabled: !!token,
+  });
 
   // Mutations para crear, actualizar y eliminar
   const createInstrument = useMutation(
@@ -76,10 +73,12 @@ const Instruments = () => {
       onSuccess: () => {
         queryClient.invalidateQueries("instruments"); // Refresca la lista de instrumentos
         toast.success("Instrumento creado exitosamente");
+        toast.clearWaitingQueue();
         setIsOpen(false);
       },
       onError: () => {
         toast.error("Error creando el instrumento");
+        toast.clearWaitingQueue();
       },
     }
   );
@@ -97,11 +96,13 @@ const Instruments = () => {
       onSuccess: () => {
         queryClient.invalidateQueries("instruments");
         toast.success("Instrumento actualizado exitosamente");
+        toast.clearWaitingQueue();
         setEditInstrument(null);
         setIsOpen(false);
       },
       onError: () => {
         toast.error("Error actualizando el instrumento");
+        toast.clearWaitingQueue();
       },
     }
   );
@@ -115,9 +116,32 @@ const Instruments = () => {
       onSuccess: () => {
         queryClient.invalidateQueries("instruments");
         toast.success("Instrumento eliminado exitosamente");
+        toast.clearWaitingQueue();
       },
       onError: () => {
         toast.error("Error eliminando el instrumento");
+        toast.clearWaitingQueue();
+      },
+    }
+  );
+
+  const deleteInstruments = useMutation(
+    (ids: string[]) =>
+      axios.delete(`${URL_BACKEND}instruments`, {
+        headers: { 
+          Authorization: `Bearer ${token}`
+        },
+        data: { ids },
+      }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("instruments");
+        toast.success("Instrumentos eliminados exitosamente");
+        toast.clearWaitingQueue();
+      },
+      onError: () => {
+        toast.error("Error eliminando los instrumentos");
+        toast.clearWaitingQueue();
       },
     }
   );
@@ -146,6 +170,8 @@ const Instruments = () => {
       item.instrumentName.toLowerCase().includes(search.toLowerCase())
     ),
   };
+
+  const heightRow: number = 52;
 
   const THEME = {
     Table: `
@@ -192,7 +218,7 @@ const Instruments = () => {
       border-right: 1px solid white;
   
       padding: 8px;
-      height: 52px;
+      height: ${heightRow}px;
 
       text-align: center;
   
@@ -201,8 +227,14 @@ const Instruments = () => {
       }
     `,
     HeaderCell: `
+      
       div div{
         justify-content: center;
+        gap: 10px;
+      }
+
+      div div span svg{
+        fill: #cacaca;
       }
     `,
     Cell: ``,
@@ -243,18 +275,17 @@ const Instruments = () => {
     console.log(action, state);
   }
 
-
   // Si hay error o está cargando
   if (isLoading) return <Loader />;
   if (isError) return <p>Error cargando instrumentos.</p>;
 
   return (
-    <div className="p-4">
-      <ToastContainer theme="dark" />
-      <div className="flex justify-between items-center mb-4">
+    <section className="p-4 flex flex-col gap-6">
+      <div className="flex justify-between items-center">
         <h1 className="text-6xl font-bold">Instrumentos</h1>
       </div>
-      <div className="pt-12 pb-6 flex flex-col gap-3 md:flex-row items-center justify-between">
+
+      <div className="pt-6 flex flex-col gap-3 md:flex-row items-center justify-between">
         <div className="flex gap-2">
           <input
             type="text"
@@ -263,19 +294,17 @@ const Instruments = () => {
             onChange={handleSearch}
             className="input-primary w-full max-w-60"
           />
-          {
-            selectedIds.length > 0 ? (
-              <button
-                onClick={() => {
-                  selectedIds.map((id: string) => deleteInstrument.mutate(id));
-                  select.fns.onRemoveAll();
-                }}
-                className="px-4 py-2 bg-red-600 text-white rounded"
-              >
-                Eliminar
-              </button>
-            ) : null
-          }
+          {selectedIds.length > 0 ? (
+            <button
+              onClick={() => {
+                deleteInstruments.mutate(selectedIds);
+                select.fns.onRemoveAll();
+              }}
+              className="px-4 py-2 bg-red-600 text-white rounded text-nowrap"
+            >
+              Eliminar {selectedIds.length}
+            </button>
+          ) : null}
         </div>
         <button
           onClick={() => {
@@ -289,104 +318,113 @@ const Instruments = () => {
         </button>
       </div>
 
-      {/* Tabla de instrumentos */}
-      <Table
-        data={data}
-        theme={theme}
-        layout={{ fixedHeader: true, horizontalScroll: true, custom: true }}
-        sort={sort}
-        select={select}
-        pagination={pagination}
+      {/* Tabla */}
+      <div
+        style={{
+          minHeight: `${heightRow * 11}px`,
+        }}
       >
-        {(tableList: Instrument[]) => (
-          <>
-            <Header>
-              <HeaderRow>
-                <HeaderCellSelect />
-                <HeaderCellSort sortKey="ID">ID</HeaderCellSort>
-                <HeaderCellSort  sortKey="Instrument">
-                  Instrumento
-                </HeaderCellSort>
-                <HeaderCellSort sortKey="">Acciones</HeaderCellSort>
-              </HeaderRow>
-            </Header>
+        <Table
+          data={data}
+          theme={theme}
+          layout={{ fixedHeader: true, horizontalScroll: true, custom: true }}
+          sort={sort}
+          select={select}
+          pagination={pagination}
+        >
+          {(tableList: Instrument[]) => (
+            <>
+              <Header>
+                <HeaderRow>
+                  <HeaderCellSelect />
+                  <HeaderCellSort sortKey="ID">ID</HeaderCellSort>
+                  <HeaderCellSort sortKey="Instrument">
+                    Instrumento
+                  </HeaderCellSort>
+                  <HeaderCellSort sortKey="">Acciones</HeaderCellSort>
+                </HeaderRow>
+              </Header>
 
-            <Body>
-              {tableList ? (
-                tableList.map((item) => (
-                  <Row key={item.id} item={item}>
-                    <CellSelect  item={item}/>
-                    <Cell>{item.id}</Cell>
-                    <Cell >{item.instrumentName}</Cell>
-                    <Cell>
-                      <div className="flex gap-1 justify-center">
-                        <button
-                          onClick={() => {
-                            setEditInstrument(item);
-                            setIsOpen(true);
-                            setValue("instrumentName", item.instrumentName);
-                          }}
-                          className="p-1 text-blue-400"
-                          title="Editar"
-                        >
-                          <svg
-                            className="feather feather-edit"
-                            fill="red"
-                            height="24"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            viewBox="0 0 24 24"
-                            width="20"
-                            xmlns="http://www.w3.org/2000/svg"
+              <Body>
+                {tableList ? (
+                  tableList.map((item) => (
+                    <Row key={item.id} item={item}>
+                      <CellSelect item={item} />
+                      <Cell>{item.id}</Cell>
+                      <Cell>{item.instrumentName}</Cell>
+                      <Cell>
+                        <div className="flex gap-1 justify-center">
+                          <button
+                            onClick={() => {
+                              setEditInstrument(item);
+                              setIsOpen(true);
+                              setValue("instrumentName", item.instrumentName);
+                            }}
+                            className="p-1 text-blue-400"
+                            title="Editar"
                           >
-                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-                          </svg>
-                        </button>
-                        <button
-                          onClick={() => deleteInstrument.mutate(item.id)}
-                          className="p-1"
-                          title="Eliminar"
-                        >
-                          <svg
-                            width="24px"
-                            height="24px"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M12.0004 9.5L17.0004 14.5M17.0004 9.5L12.0004 14.5M4.50823 13.9546L7.43966 17.7546C7.79218 18.2115 7.96843 18.44 8.18975 18.6047C8.38579 18.7505 8.6069 18.8592 8.84212 18.9253C9.10766 19 9.39623 19 9.97336 19H17.8004C18.9205 19 19.4806 19 19.9084 18.782C20.2847 18.5903 20.5907 18.2843 20.7824 17.908C21.0004 17.4802 21.0004 16.9201 21.0004 15.8V8.2C21.0004 7.0799 21.0004 6.51984 20.7824 6.09202C20.5907 5.71569 20.2847 5.40973 19.9084 5.21799C19.4806 5 18.9205 5 17.8004 5H9.97336C9.39623 5 9.10766 5 8.84212 5.07467C8.6069 5.14081 8.38579 5.2495 8.18975 5.39534C7.96843 5.55998 7.79218 5.78846 7.43966 6.24543L4.50823 10.0454C3.96863 10.7449 3.69883 11.0947 3.59505 11.4804C3.50347 11.8207 3.50347 12.1793 3.59505 12.5196C3.69883 12.9053 3.96863 13.2551 4.50823 13.9546Z"
-                              stroke="#ff3e3e"
-                              strokeWidth="2"
+                            <svg
+                              className="feather feather-edit"
+                              fill="red"
+                              height="24"
+                              stroke="currentColor"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </Cell>
+                              strokeWidth="2"
+                              viewBox="0 0 24 24"
+                              width="20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                              <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() => deleteInstrument.mutate(item.id)}
+                            className="p-1"
+                            title="Eliminar"
+                          >
+                            <svg
+                              width="24px"
+                              height="24px"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M12.0004 9.5L17.0004 14.5M17.0004 9.5L12.0004 14.5M4.50823 13.9546L7.43966 17.7546C7.79218 18.2115 7.96843 18.44 8.18975 18.6047C8.38579 18.7505 8.6069 18.8592 8.84212 18.9253C9.10766 19 9.39623 19 9.97336 19H17.8004C18.9205 19 19.4806 19 19.9084 18.782C20.2847 18.5903 20.5907 18.2843 20.7824 17.908C21.0004 17.4802 21.0004 16.9201 21.0004 15.8V8.2C21.0004 7.0799 21.0004 6.51984 20.7824 6.09202C20.5907 5.71569 20.2847 5.40973 19.9084 5.21799C19.4806 5 18.9205 5 17.8004 5H9.97336C9.39623 5 9.10766 5 8.84212 5.07467C8.6069 5.14081 8.38579 5.2495 8.18975 5.39534C7.96843 5.55998 7.79218 5.78846 7.43966 6.24543L4.50823 10.0454C3.96863 10.7449 3.69883 11.0947 3.59505 11.4804C3.50347 11.8207 3.50347 12.1793 3.59505 12.5196C3.69883 12.9053 3.96863 13.2551 4.50823 13.9546Z"
+                                stroke="#ff3e3e"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </Cell>
+                    </Row>
+                  ))
+                ) : (
+                  <Row item={{ id: "", instrumentName: "" }}>
+                    <Cell>No hay instrumentos</Cell>
                   </Row>
-                ))
-              ) : (
-                <Row item={{ id: "", instrumentName: "" }}>
-                  <Cell>No hay instrumentos</Cell>
-                </Row>
-              )}
-            </Body>
-          </>
-        )}
-      </Table>
+                )}
+              </Body>
+            </>
+          )}
+        </Table>
+      </div>
 
-      <br />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <span>Total Pages: {pagination.state.getTotalPages(data.nodes)}</span>
-
+      <div className="flex justify-between">
+        <span>
+          Pagina {pagination.state.page + 1} de{" "}
+          {pagination.state.getTotalPages(data.nodes)}
+        </span>
         <div className="flex gap-2">
           <button
-            className={`${pagination.state.page === 0 ? "bg-gray-500": "bg-primary"} p-3 rounded`}
+            className={`${
+              pagination.state.page === 0 ? "bg-gray-500" : "bg-primary"
+            } p-3 rounded`}
             type="button"
             disabled={pagination.state.page === 0}
             onClick={() => pagination.fns.onSetPage(pagination.state.page - 1)}
@@ -409,7 +447,12 @@ const Instruments = () => {
             </svg>
           </button>
           <button
-            className={`${pagination.state.page + 1 === pagination.state.getTotalPages(data.nodes) ? "bg-gray-500": "bg-primary"} p-3 rounded`}
+            className={`${
+              pagination.state.page + 1 ===
+              pagination.state.getTotalPages(data.nodes)
+                ? "bg-gray-500"
+                : "bg-primary"
+            } p-3 rounded`}
             type="button"
             disabled={
               pagination.state.page + 1 ===
@@ -447,7 +490,7 @@ const Instruments = () => {
           <div className="fixed inset-0 bg-principal-bg bg-opacity-80" />
           <div className="fixed inset-0 flex items-center justify-center p-4">
             <DialogPanel className="bg-principal-bg border border-white border-opacity-40 rounded-lg p-6 w-full max-w-md">
-              <DialogTitle  className="text-lg font-bold">
+              <DialogTitle className="text-lg font-bold">
                 {editInstrument ? "Editar Instrumento" : "Crear Instrumento"}
               </DialogTitle>
               <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
@@ -482,7 +525,12 @@ const Instruments = () => {
           </div>
         </Dialog>
       )}
-    </div>
+
+      <ToastContainer
+        theme="dark"
+        limit={1}
+      />
+    </section>
   );
 };
 
