35468d06cb9ad17214d136969ff706ba2d06821a
Daniel Amado <daniel.amadove@gmail.com>
2024-10-26 10:16:12 -0500

    Agregar archivo DaysOfWeek.ts con los días de la semana en español y en ingles

---
+++ frontend/src/data/DaysOfWeek.ts
@@ -0,0 +1,11 @@
+import { DayOfWeek } from "../types/Api";
+
+export const DayOfWeeks: DayOfWeek[] = [
+  { id: 1, name: "Monday", displayName: "Lunes" },
+  { id: 2, name: "Tuesday", displayName: "Martes" },
+  { id: 3, name: "Wednesday", displayName: "Miércoles" },
+  { id: 4, name: "Thursday", displayName: "Jueves" },
+  { id: 5, name: "Friday", displayName: "Viernes" },
+  { id: 6, name: "Saturday", displayName: "Sábado" },
+  { id: 7, name: "Sunday", displayName: "Domingo" }
+];
--- frontend/src/types/Api.d.ts
+++ frontend/src/types/Api.d.ts
@@ -1,7 +1,13 @@
 // types/Api.d.ts
-export type ResourceType = 'instruments' | 'rooms' | 'semesters' | 'professors' | 'students';
+export type DayOfWeekName = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
+export type ResourceType = 'instruments' | 'rooms' | 'courses';
 
-// Extiende TableNode
+type DayOfWeek = {
+  id: number;
+  name: DayOfWeekName;       // Nombre en inglés
+  displayName: string; // Nombre en español
+};
+
 export interface InstrumentType{
   id: string;
   instrumentName: string;
@@ -11,4 +17,18 @@ type Room = {
   id: string;
   name: string;
   capacity: number;
+};
+
+type CourseAvailability = {
+  dayOfWeek: string;      // Ej: 'Monday', 'Tuesday', etc.
+  startTime: string | null; // Formato de tiempo o null si no se especifica
+  endTime: string | null;   // Formato de tiempo o null si no se especifica
+};
+
+type Course = {
+  id: number;                    // ID del curso
+  name: string;                  // Nombre del curso
+  description: string | null;    // Descripción, puede ser null si no se especifica
+  isOnline: boolean;             // Indica si el curso es online
+  availability: CourseAvailability[]; // Arreglo de disponibilidades del curso
 };
